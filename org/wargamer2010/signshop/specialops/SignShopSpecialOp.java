5321669d567b2f418184b908366c13fa28efee18
wargamer <wargamer2010@outlook.com>
2012-06-18 23:43:07 +0200

    Added missing files from last commit

---
+++ org/wargamer2010/signshop/specialops/SignShopSpecialOp.java
@@ -0,0 +1,9 @@
+package org.wargamer2010.signshop.specialops;
+
+import org.bukkit.block.Block;
+import org.bukkit.event.player.PlayerInteractEvent;
+import java.util.List;
+
+public interface SignShopSpecialOp {
+    public Boolean runOperation(List<Block> clickedBlocks, PlayerInteractEvent event);
+}
