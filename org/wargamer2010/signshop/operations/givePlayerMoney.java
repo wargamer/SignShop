646f4c1063a08077eb647ed5845a8b2f83e7253b
wargamer <wargamer2010@outlook.com>
2012-09-16 20:37:26 +0200

    Updates Quick reference and fixed a bug related to price modifiers not being properly reflected in messages for iSell shops

--- org/wargamer2010/signshop/operations/givePlayerMoney.java
+++ org/wargamer2010/signshop/operations/givePlayerMoney.java
@@ -10,16 +10,18 @@ public class givePlayerMoney implements SignShopOperation {
     }
     
     @Override
-    public Boolean checkRequirements(SignShopArguments ssArgs, Boolean activeCheck) {        
-        ssArgs.set_fPrice(ssArgs.get_fPrice());
-        ssArgs.setMessagePart("!price", economyUtil.formatMoney(ssArgs.get_fPrice()));
+    public Boolean checkRequirements(SignShopArguments ssArgs, Boolean activeCheck) {                
+        Float fPricemod = ssArgs.get_ssPlayer().getPlayerPricemod(ssArgs.get_sOperation(), false);
+        Float fPrice = (ssArgs.get_fPrice_root() * fPricemod);        
+        ssArgs.set_fPrice(fPrice);
+        ssArgs.setMessagePart("!price", economyUtil.formatMoney(fPrice));                
         return true;
     }
     
     @Override
     public Boolean runOperation(SignShopArguments ssArgs) {
         Float fPricemod = ssArgs.get_ssPlayer().getPlayerPricemod(ssArgs.get_sOperation(), false);
-        Float fPrice = (ssArgs.get_fPrice() * fPricemod);
+        Float fPrice = (ssArgs.get_fPrice_root() * fPricemod);
         Boolean bTransaction = ssArgs.get_ssPlayer().mutateMoney(fPrice);
         if(!bTransaction)
             ssArgs.get_ssPlayer().sendMessage("The money transaction failed, please contact the System Administrator");
