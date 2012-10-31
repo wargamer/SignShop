5421930c7c088002e3f005d1c60374b80b87670b
wargamer <wargamer2010@outlook.com>
2012-10-31 22:29:12 +0100

    Added a missing NULL check in spoutUtil which caused an NPE for some non-Spout materials.

--- org/wargamer2010/signshop/util/spoutUtil.java
+++ org/wargamer2010/signshop/util/spoutUtil.java
@@ -1,10 +1,21 @@
 package org.wargamer2010.signshop.util;
 
+import org.getspout.spoutapi.material.Material;
 import org.getspout.spoutapi.material.MaterialData;
 
 
 public class spoutUtil {
-    public static String getName(org.bukkit.material.MaterialData data, Short durability) {                     
-        return MaterialData.getMaterial(data.getItemTypeId(), durability).getName();        
+    private spoutUtil() {
+
+    }
+
+    public static String getName(org.bukkit.material.MaterialData data, Short durability) {
+        if(data == null)
+            return null;
+        Material spoutmat = MaterialData.getMaterial(data.getItemTypeId(), durability);
+        if(spoutmat != null)
+            return spoutmat.getName();
+        else
+            return null;
     }
 }
