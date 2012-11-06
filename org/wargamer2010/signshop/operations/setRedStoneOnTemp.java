ec5ccc236e73d9ca172ec6d14b58c1dbc16c169e
wargamer <wargamer2010@outlook.com>
2012-11-06 19:55:33 +0100

    Fixed an issue where the redstonetemp task would not finish when the plugin is reloaded.

--- org/wargamer2010/signshop/operations/setRedStoneOnTemp.java
+++ org/wargamer2010/signshop/operations/setRedStoneOnTemp.java
@@ -11,28 +11,28 @@ import org.wargamer2010.signshop.configuration.SignShopConfig;
 import org.wargamer2010.signshop.util.lagSetter;
 import org.wargamer2010.signshop.SignShop;
 
-public class setRedStoneOnTemp implements SignShopOperation {    
+public class setRedStoneOnTemp implements SignShopOperation {
     @Override
     public Boolean setupOperation(SignShopArguments ssArgs) {
         Boolean foundLever = false;
         for(Block block : ssArgs.get_activatables())
             if(block.getType() == Material.getMaterial("LEVER"))
                 foundLever = true;
-        if(!foundLever) {            
+        if(!foundLever) {
             ssArgs.get_ssPlayer().sendMessage(SignShopConfig.getError("lever_missing", ssArgs.messageParts));
             return false;
         }
         return true;
     }
-    
+
     @Override
-    public Boolean checkRequirements(SignShopArguments ssArgs, Boolean activeCheck) {        
+    public Boolean checkRequirements(SignShopArguments ssArgs, Boolean activeCheck) {
         Boolean bReturn = false;
-        Block bLever = null; 
-        
-        for(int i = 0; i < ssArgs.get_activatables().size(); i++) {            
+        Block bLever = null;
+
+        for(int i = 0; i < ssArgs.get_activatables().size(); i++) {
             bLever = ssArgs.get_activatables().get(i);
-            
+
             if(bLever.getType() == Material.getMaterial("LEVER")) {
                 BlockState state = bLever.getState();
                 MaterialData data = state.getData();
@@ -45,22 +45,22 @@ public class setRedStoneOnTemp implements SignShopOperation {
             ssArgs.get_ssPlayer().sendMessage(SignShopConfig.getError("already_on", ssArgs.messageParts));
         return bReturn;
     }
-    
+
     @Override
     public Boolean runOperation(SignShopArguments ssArgs) {
         Block bLever = null;
-        
+
         Integer delay = 20;
-        if(!ssArgs.getFirstOperationParameter().equals("")) {            
+        if(!ssArgs.getFirstOperationParameter().equals("")) {
             try {
                 delay = Integer.parseInt(ssArgs.getFirstOperationParameter());
                 if(delay <= 0)
                     delay = 20;
             } catch(NumberFormatException ex) {
-                
+
             }
         }
-        
+
         for(int i = 0; i < ssArgs.get_activatables().size(); i++) {
             bLever = ssArgs.get_activatables().get(i);
             if(bLever.getType() == Material.getMaterial("LEVER")) {
@@ -68,14 +68,14 @@ public class setRedStoneOnTemp implements SignShopOperation {
                 MaterialData data = state.getData();
                 Lever lever = (Lever)data;
                 if(!lever.isPowered()) {
-                    lever.setPowered(true);                
+                    lever.setPowered(true);
                     state.setData(lever);
                     state.update();
                     signshopUtil.generateInteractEvent(bLever, ssArgs.get_ssPlayer().getPlayer(), ssArgs.get_bfBlockFace());
-                    Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(SignShop.getInstance(),new lagSetter(bLever),10*delay);
-                }            
+                    Bukkit.getServer().getScheduler().scheduleAsyncDelayedTask(SignShop.getInstance(),new lagSetter(bLever),10*delay);
+                }
             }
-        }    
+        }
 
         return true;
     }
