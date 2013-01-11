b94fb747cc0c2e8f4ba168b4e745bea91fb4a4a7
wargamer <wargamer2010@outlook.com>
2013-01-11 21:33:28 +0100

    Extended Potion support to include stock potions.

--- org/wargamer2010/signshop/util/itemUtil.java
+++ org/wargamer2010/signshop/util/itemUtil.java
@@ -107,7 +107,7 @@ public class itemUtil {
         }
     }
 
-    private static String binaryToRoman(int binary) {
+    public static String binaryToRoman(int binary) {
         final String[] RCODE = {"M", "CM", "D", "CD", "C", "XC", "L",
                                            "XL", "X", "IX", "V", "IV", "I"};
         final int[]    BVAL  = {1000, 900, 500, 400,  100,   90,  50,
