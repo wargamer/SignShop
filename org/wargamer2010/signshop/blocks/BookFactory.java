0cb9b4352c2e6cc17de29d5d1c96e9206296b0bc
Vincent Spaa <vincentspaa@hotmail.com>
2013-04-06 20:17:02 +0200

    Completely fased out dirty reflection based version checking and moved to proper version number comparing.

--- org/wargamer2010/signshop/blocks/BookFactory.java
+++ org/wargamer2010/signshop/blocks/BookFactory.java
@@ -1,20 +1,17 @@
 
 package org.wargamer2010.signshop.blocks;
 
-import java.util.logging.Level;
-import org.wargamer2010.signshop.SignShop;
+import org.wargamer2010.signshop.util.SSBukkitVersion;
+import org.wargamer2010.signshop.util.versionUtil;
 
 public class BookFactory {
-    private static String namespaceType = "";
-    
     private BookFactory() {
 
     }
 
     public static IBookItem getBookItem(org.bukkit.inventory.ItemStack stack) {
-        String useType = getNamespaceType();
         IBookItem item;
-        if(useType.equals("pre145"))
+        if(versionUtil.getBukkitVersionType() == SSBukkitVersion.Pre145)
             item = new org.wargamer2010.signshop.blocks.BookItem(stack);
         else
             item = new org.wargamer2010.signshop.blocks.v145.BookItem(stack);
@@ -22,32 +19,12 @@ public class BookFactory {
     }
 
     public static IItemTags getItemTags() {
-        String useType = getNamespaceType();
         IItemTags tags;
-        if(useType.equals("pre145"))
+        if(versionUtil.getBukkitVersionType() == SSBukkitVersion.Pre145)
             tags = new org.wargamer2010.signshop.blocks.itemTags();
         else
             tags = new org.wargamer2010.signshop.blocks.v145.itemTags();
 
         return tags;
-    }
-
-    protected static String getNamespaceType() {
-        if(namespaceType.isEmpty()) {
-            if(tryReflection("org.bukkit.craftbukkit.inventory.CraftItemStack"))
-                namespaceType = "pre145";
-            else
-                namespaceType = "post145";
-        }
-        return namespaceType;
-    }
-
-    protected static boolean tryReflection(String fullClassname) {
-        try {
-            Class.forName(fullClassname);
-            return true;
-        } catch (ClassNotFoundException ex) { }
-
-        return false;
     }
 }
