b5e8ea9eeea26eeffee52dfcba1c0a68244f759b
Vincent Spaa <vincentspaa@hotmail.com>
2013-06-02 22:45:40 +0200

    Corrected implementation of Towny and WG build limitation

--- org/wargamer2010/signshop/listeners/sslisteners/TownyChecker.java
+++ org/wargamer2010/signshop/listeners/sslisteners/TownyChecker.java
@@ -1,9 +1,6 @@
 
 package org.wargamer2010.signshop.listeners.sslisteners;
 
-import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
-import com.palmergames.bukkit.towny.object.Resident;
-import com.palmergames.bukkit.towny.object.Town;
 import com.palmergames.bukkit.towny.object.TownBlock;
 import com.palmergames.bukkit.towny.object.TownBlockType;
 import com.palmergames.bukkit.towny.object.TownyUniverse;
@@ -23,21 +20,17 @@ public class TownyChecker implements Listener {
         if(HookManager.getHook("Towny") == null)
             return;
 
+        // Towny will take of plot ownership so no need to check that here
         TownBlock block = TownyUniverse.getTownBlock(event.getSign().getLocation());
-        if(block == null || block.getType() != TownBlockType.COMMERCIAL) // Commercial == ShopPlot
-            return;
-
-        // If the player is not part of a Town, not registered as a Resident or part of the wrong town
-        // we'll assume Towny has already properly handled it so we we only check for permission here
-        if(event.getPlayer().hasPerm("SignShop.Towny.BuildOnShopPlot", true))
-            return;
-        if(event.getPlayer().isOp())
-            event.getPlayer().sendMessage(SignShopConfig.getError("towny_shop_plot_not_allowed_but_op", event.getMessageParts()));
-        else if(event.getPlayer().hasBypassShopPlots("Towny"))
-            event.getPlayer().sendMessage(SignShopConfig.getError("towny_shop_plot_not_allowed_but_perm", event.getMessageParts()));
-        else {
-            event.getPlayer().sendMessage(SignShopConfig.getError("towny_shop_plot_not_allowed", event.getMessageParts()));
-            event.setCancelled(true);
+        if(block == null || block.getType() != TownBlockType.COMMERCIAL) { // Commercial == ShopPlot
+            if(event.getPlayer().isOp())
+                event.getPlayer().sendMessage(SignShopConfig.getError("towny_shop_plot_not_allowed_but_op", event.getMessageParts()));
+            else if(event.getPlayer().hasBypassShopPlots("Towny"))
+                event.getPlayer().sendMessage(SignShopConfig.getError("towny_shop_plot_not_allowed_but_perm", event.getMessageParts()));
+            else {
+                event.getPlayer().sendMessage(SignShopConfig.getError("towny_shop_plot_not_allowed", event.getMessageParts()));
+                event.setCancelled(true);
+            }
         }
     }
 }
