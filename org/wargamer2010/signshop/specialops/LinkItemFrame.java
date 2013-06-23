f40888dd9b9b505f097979f1a9cff2dc650c4784
wargamer <wargamer2010@outlook.com>
2013-06-23 12:29:46 +0200

    Allowed for multiple itemframes to be linked and fixed buggy location compare

--- org/wargamer2010/signshop/specialops/LinkItemFrame.java
+++ org/wargamer2010/signshop/specialops/LinkItemFrame.java
@@ -1,5 +1,6 @@
 package org.wargamer2010.signshop.specialops;
 
+import java.util.LinkedList;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -7,6 +8,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import java.util.List;
 import java.util.Map;
 import org.bukkit.Material;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.ItemFrame;
 import org.wargamer2010.signshop.Seller;
@@ -15,8 +17,11 @@ import org.wargamer2010.signshop.util.*;
 
 import org.wargamer2010.signshop.configuration.SignShopConfig;
 import org.wargamer2010.signshop.configuration.Storage;
+import org.wargamer2010.signshop.operations.SignShopArguments;
 
 public class LinkItemFrame implements SignShopSpecialOp {
+    private static final String MiscSetting = "itemframelocation";
+
     @Override
     public Boolean runOperation(List<Block> clickedBlocks, PlayerInteractEvent event, Boolean ranSomething) {
         if(Material.getMaterial("ITEM_FRAME") == null)
@@ -32,12 +37,13 @@ public class LinkItemFrame implements SignShopSpecialOp {
         if(!itemUtil.clickedSign(shopSign))
             return false;
 
-        ItemFrame itemframe = null;
+
+        List<ItemFrame> itemframes = new LinkedList<ItemFrame>();
         for(Map.Entry<Entity, Player> entry : clicks.mClicksPerEntity.entrySet()) {
             if(entry.getValue() == player && entry.getKey() instanceof ItemFrame)
-                itemframe = (ItemFrame)entry.getKey();
+                itemframes.add((ItemFrame)entry.getKey());
         }
-        if(itemframe == null)
+        if(itemframes.isEmpty())
             return false;
 
         ItemStack showcasing;
@@ -48,10 +54,46 @@ public class LinkItemFrame implements SignShopSpecialOp {
         showcasing = seller.getItems()[0];
         clicks.removePlayerFromEntityMap(player);
 
-        itemframe.setItem(showcasing);
+        List<Entity> newEntities = new LinkedList<Entity>();
+        List<Entity> currentEntities = signshopUtil.getEntitiesFromMisc(seller, MiscSetting);
+        Boolean bUnlinked = false;
+        for(ItemFrame bTemp : itemframes) {
+            if(currentEntities.contains(bTemp)) {
+                ssPlayer.sendMessage(SignShopConfig.getError("ItemFrame has been successfully unlinked.", null));
+                bUnlinked = true;
+                currentEntities.remove(bTemp);
+                bTemp.setItem(null);
+            } else {
+                newEntities.add(bTemp);
+                bTemp.setItem(showcasing);
+            }
+        }
 
-        seller.getMisc().put("itemframelocation", signshopUtil.convertLocationToString(itemframe.getLocation()));
+        if(!bUnlinked && newEntities.isEmpty())
+            return false;
+        else if(newEntities.isEmpty()) {
+            seller.getMisc().remove(MiscSetting);
+            Storage.get().SafeSave();
+            return true;
+        }
+        currentEntities.addAll(newEntities);
+
+        List<String> entityLocations = new LinkedList<String>();
+        for(Entity ent : currentEntities)
+            entityLocations.add(signshopUtil.convertLocationToString(ent.getLocation()));
+        String[] implodedLocations = new String[entityLocations.size()];
+        entityLocations.toArray(implodedLocations);
+        String locations = signshopUtil.implode(implodedLocations, SignShopArguments.seperator);
+
+        if(locations.isEmpty())
+            return true;
+
         ssPlayer.sendMessage("ItemFrame has been successfully linked.");
+        seller.getMisc().put(MiscSetting, locations);
+        Storage.get().SafeSave();
+
+        // seller.getMisc().put("itemframelocation", signshopUtil.convertLocationToString(itemframe.getLocation()));
+        // ssPlayer.sendMessage("ItemFrame has been successfully linked.");
         return true;
     }
 }
