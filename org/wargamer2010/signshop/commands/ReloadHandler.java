c873ff682e3f5458f650b713069bcf972d833621
Vincent Spaa <vincentspaa@hotmail.com>
2013-09-10 19:34:03 +0200

    Added reload mechanism for dependent plugins.

--- org/wargamer2010/signshop/commands/ReloadHandler.java
+++ org/wargamer2010/signshop/commands/ReloadHandler.java
@@ -4,6 +4,8 @@ package org.wargamer2010.signshop.commands;
 import java.util.logging.Level;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.wargamer2010.signshop.SignShop;
 import org.wargamer2010.signshop.player.SignShopPlayer;
 import org.wargamer2010.signshop.util.signshopUtil;
@@ -23,9 +25,23 @@ public class ReloadHandler implements ICommandHandler {
     public boolean handle(String command, String[] args, SignShopPlayer player) {
         if(!signshopUtil.hasOPForCommand(player))
             return true;
+        PluginManager pm = Bukkit.getPluginManager();
 
-        Bukkit.getServer().getPluginManager().disablePlugin(SignShop.getInstance());
-        Bukkit.getServer().getPluginManager().enablePlugin(SignShop.getInstance());
+        pm.disablePlugin(SignShop.getInstance());
+        pm.enablePlugin(SignShop.getInstance());
+
+        // Reload hard dependencies
+        for(Plugin plugin : pm.getPlugins()) {
+            if(plugin != null && plugin.getDescription() != null && plugin.isEnabled() && plugin.getDescription().getDepend() != null) {
+                for(String depend : plugin.getDescription().getDepend()) {
+                    if(depend.equalsIgnoreCase("signshop")) {
+                        pm.disablePlugin(plugin);
+                        pm.enablePlugin(plugin);
+                    }
+                }
+            }
+        }
+
         SignShop.log("Reloaded", Level.INFO);
         if(player != null)
             player.sendMessage(ChatColor.GREEN + "SignShop has been reloaded");
