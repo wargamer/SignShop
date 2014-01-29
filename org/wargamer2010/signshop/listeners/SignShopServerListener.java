60438b28c7494cdb6bb56187e2c2d29d036e08e7
wargamer <wargamer2010@outlook.com>
2014-01-29 19:50:13 +0100

    Fixed classnotfoundexception when Essentials is not installed on the server

--- org/wargamer2010/signshop/listeners/SignShopServerListener.java
+++ org/wargamer2010/signshop/listeners/SignShopServerListener.java
@@ -1,82 +1,37 @@
 package org.wargamer2010.signshop.listeners;
 
-import org.bukkit.event.server.PluginDisableEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.Server;
-import java.util.logging.Level;
-
-import org.bukkit.configuration.file.FileConfiguration;
-
-import com.earth2me.essentials.Essentials;
-import com.earth2me.essentials.ISettings;
-import com.earth2me.essentials.signs.EssentialsSign;
-import org.bukkit.ChatColor;
-import org.wargamer2010.signshop.SignShop;
-import org.wargamer2010.signshop.configuration.SignShopConfig;
-import org.wargamer2010.signshop.util.signshopUtil;
 
 public class SignShopServerListener implements Listener {
     private Server server;
-    private Essentials ess = null;
-    public static Boolean essConflictFound = false;
+    private static final String pluginName = "Essentials";
 
     public SignShopServerListener(Server pServer) {
         server = pServer;
         setupPluginToHookInto();
     }
 
+    public static boolean isEssentialsConflictFound() {
+        if(Bukkit.getServer().getPluginManager().getPlugin(pluginName) != null)
+            return EssentialsHelper.isEssentialsConflictFound();
+        return false;
+    }
+
     @EventHandler(priority = EventPriority.MONITOR)
     public void onPluginEnabled(PluginEnableEvent event) {
-        if(event.getPlugin().getName().equals("Essentials"))            
+        if(event.getPlugin().getName().equals(pluginName))
             setupPluginToHookInto();
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onPluginDisabled(PluginDisableEvent event) {
-        if(event.getPlugin().getName().equals("Essentials"))
-            this.ess = null;        
-    }
-
     final public void setupPluginToHookInto() {
-        Plugin plugin = this.server.getPluginManager().getPlugin("Essentials");
-
-        if (plugin != null) {
-            this.ess = (Essentials)plugin;
-            essentialsCheck();
-        }        
-    }
-
-    public void essentialsCheck() {
-        if(this.ess != null) {
-            ISettings settings = this.ess.getSettings();
-            if(settings == null) {
-                this.ess = null;
-                return;
-            }
-            if(!settings.areSignsDisabled()) {
-                SignShop.log("Essentials signs are enabled, checking for conflicts now!", Level.WARNING);
-                FileConfiguration config = this.ess.getConfig();
-                if(config == null) {
-                    this.ess = null;
-                    return;
-                }       
-                essConflictFound = false;
-                for(EssentialsSign sign : this.ess.getSettings().enabledSigns()) {
-                    String essShopName = signshopUtil.getOperation(sign.getTemplateName());
-                    if(essShopName.isEmpty())
-                        continue;
-                    essShopName = ChatColor.stripColor(essShopName).toLowerCase();
-                    if(!SignShopConfig.getBlocks(essShopName).isEmpty()) {
-                        SignShop.log("Sign with name " + sign.getTemplateName() + " is enabled for Essentials and conflicts with SignShop!", Level.SEVERE);
-                        if(!essConflictFound)
-                            essConflictFound = true;
-                    }
-                }                
-            }
-        }
+        Plugin plugin = this.server.getPluginManager().getPlugin(pluginName);
+        if (plugin != null)
+            EssentialsHelper.essentialsCheck(plugin);
     }
 }
