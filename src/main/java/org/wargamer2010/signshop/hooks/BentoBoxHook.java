2dd7411b226cd4cbd7024b35906a5c98b8a2ebf2
Weaves <weaves7@gmail.com>
2020-04-23 23:43:44 -0500

    Op/SuperAdmin can bypass BentoBox protection

--- src/main/java/org/wargamer2010/signshop/hooks/BentoBoxHook.java
+++ src/main/java/org/wargamer2010/signshop/hooks/BentoBoxHook.java
@@ -2,6 +2,7 @@ package org.wargamer2010.signshop.hooks;
 
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.wargamer2010.signshop.player.SignShopPlayer;
 import world.bentobox.bentobox.BentoBox;
 import world.bentobox.bentobox.api.user.User;
 import world.bentobox.bentobox.database.objects.Island;
@@ -16,16 +17,22 @@ public class BentoBoxHook implements Hook {
         return "BentoBox";
     }
 
-    @Override
+    @Override //TODO Make use of bentobox flags
     public Boolean canBuild(Player player, Block block) {
-        if(HookManager.getHook("BentoBox") == null)
+        if (HookManager.getHook("BentoBox") == null) {
             return true;
+        }
+        //BentoBox does not currently have a general bypass for all gamemodes so we will check for op/superadmin
+        if (SignShopPlayer.isOp(player)) {
+            return true;
+        }
         BentoBox bentoBox = BentoBox.getInstance();
         User user = User.getInstance(player);
         IslandsManager islandsManager = bentoBox.getIslands();
         Optional<Island> island = islandsManager.getIslandAt(block.getLocation());
-        if (island.isPresent() && island.get().getMembers().containsKey(user.getUniqueId())){
-            return  island.get().getMembers().get(user.getUniqueId()) >= 500;
+        if (island.isPresent() && island.get().getMembers().containsKey(user.getUniqueId())) {
+            //visitor:0 coop:200 trusted:400 member:500 sub-owner:900 owner:1000
+            return island.get().getMembers().get(user.getUniqueId()) >= 500; // visitor:0 coop:
         }
         return false;
     }
