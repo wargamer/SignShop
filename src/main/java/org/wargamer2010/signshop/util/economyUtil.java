6a7e3d89654110ab99db598c1876db3a90a41e1c
Matthew Meredith <matthewmeredith775@gmail.com>
2022-06-26 12:25:33 -0400

    Modified the currently implemented price stripper to pass the stripped string off to the international parser. Removed regex price detection from the new parser in favor of the old parsers stripper.
    Removed references to "international" in favor of definitive "period-seperated" and "comma-seperated" descriptions
    
    Simplified code structure

--- src/main/java/org/wargamer2010/signshop/util/economyUtil.java
+++ src/main/java/org/wargamer2010/signshop/util/economyUtil.java
@@ -28,13 +28,13 @@ public class economyUtil {
     public static double parsePrice(String line) {
         if(line == null)
             return 0.0d;
-        if (SignShopConfig.useInternationalCurrencyParser()) return parsePriceInternational(line);
         String priceline = ChatColor.stripColor(line);
         StringBuilder sPrice = new StringBuilder();
         Double fPrice;
         for(int i = 0; i < priceline.length(); i++)
-            if(Character.isDigit(priceline.charAt(i)) || priceline.charAt(i) == '.')
+            if(Character.isDigit(priceline.charAt(i)) || priceline.charAt(i) == '.' || (SignShopConfig.allowCommaDecimalSeperator() && priceline.charAt(i) == ','))
                 sPrice.append(priceline.charAt(i));
+        if (SignShopConfig.allowCommaDecimalSeperator()) return parsePriceInternational(sPrice.toString());
         try {
             fPrice = Double.parseDouble(sPrice.toString());
         }
@@ -49,32 +49,13 @@ public class economyUtil {
         return fPrice;
     }
 
-    public static double parsePriceInternational(String line) {
-        if (SignShopConfig.debugging()) SignShop.debugMessage("Parsing a price from '" + line + "'");
-        /*
-            Start of String
-            NCG- All non-numeric characters up to the first digit
-            Capture Group- All sequential numerals, commas, periods, and spaces
-            NCG- Any characters after the price (having a group of numerals seperated from the price will cause the method to fail)
-        */
-        Matcher priceFinder = Pattern.compile("^[\\D]*+([\\d\\s,.]*+)[\\D]*+$").matcher(ChatColor.stripColor(line));
-
-        if (!priceFinder.matches()) {
-            if (SignShopConfig.debugging()) SignShop.debugMessage(line + " does not contain a valid price");
-            return 0.0d;
-        }
-
-        // Get the captured price from the regex
-        String price = priceFinder.group(1);
+    private static double parsePriceInternational(String price) {
+        if (SignShopConfig.debugging()) SignShop.debugMessage("Parsing a price from '" + price + "'");
 
         if (price == null || price.equals("")) {
-            if (SignShopConfig.debugging()) SignShop.debugMessage(line + " does not contain a price");
+            if (SignShopConfig.debugging()) SignShop.debugMessage("Empty! (no price found)");
             return 0.0d;
         }
-        // Strip out spaces
-        else price = price.replace(" ", "");
-
-        if (SignShopConfig.debugging()) SignShop.debugMessage("Detected price: " + price);
 
         // Count the number, and last known position of each type of delimiter
         Matcher periodFinder = Pattern.compile("[.]").matcher(price);
@@ -108,19 +89,19 @@ public class economyUtil {
                 // There are delimiters, determine what kind
 
             /*
-                If the comma comes last, and there is only one comma, it is *probably* international
-                    Sidenote: it doesn't matter if not actually comma delimited, the parser will fix that later
-                If there are no commas and more than one period, it has to be an international number
-                    An internation number cannot be valid with more than one comma,
-                    the same way that a US number cannot be valid with more than one period.
+                If the comma comes last, and there is only one comma, it is *probably* comma seperated
+                    Sidenote: it doesn't matter if not actually comma seperated, the parser will fix that later
+                If there are no commas and more than one period, it has to be a comma seperated number
+                    A comma seperated number cannot be valid with more than one comma,
+                    the same way that a period seperated number cannot be valid with more than one period.
 
                 This method does not select a guess that is theoretically impossible (contains more than one decimal seperator),
-                although it will default to attempting to parse a US delimited number
+                although it will default to attempting to parse a period seperated number
             */
-                boolean likelyInternational = (lastComma > lastPeriod && totalCommas == 1) || (totalCommas == 0 && totalPeriods > 1);
+                boolean likelyCommaSeperated = (lastComma > lastPeriod && totalCommas == 1) || (totalCommas == 0 && totalPeriods > 1);
 
                 if (SignShopConfig.debugging())
-                    SignShop.debugMessage("Likely: " + (likelyInternational ? "INTERNATIONAL" : "NOT INTERNATIONAL"));
+                    SignShop.debugMessage("Likely: " + (likelyCommaSeperated ? "COMMA SEPERATED" : "PERIOD SEPERATED"));
 
             /*
                 Start of String
@@ -132,34 +113,32 @@ public class economyUtil {
                 These regexs must match the ENTIRE string. If a complete match is not found it is considered invalid.
                 This ensures that there is only one decimal seperator. Having 2+ is not valid
 
-                US Parser uses ',' as a division, and '.' as a decimal seperator
-                EU Parser uses '.' as a division, and ',' as a decimal seperator
+                Period-seperated Parser uses ',' as a division, and '.' as a decimal seperator
+                Comma-seperated Parser uses '.' as a division, and ',' as a decimal seperator
              */
-                Matcher usParser = Pattern.compile("^([\\d,]*+)(?:[.](\\d{0,2}))?$").matcher(price);
-                Matcher euParser = Pattern.compile("^([\\d.]*+)(?:[,](\\d{0,2}))?$").matcher(price);
+                Matcher periodSeperatedParser = Pattern.compile("^([\\d,]*+)(?:[.](\\d{0,2}))?$").matcher(price);
+                Matcher commaSeperatedParser = Pattern.compile("^([\\d.]*+)(?:[,](\\d{0,2}))?$").matcher(price);
 
-                String usPriceString = usParser.matches() ? price.replace(",", "") : null;
-                String euPriceString = euParser.matches() ? price.replace(".", "").replace(",", ".") : null;
+                String periodSeperatedString = periodSeperatedParser.matches() ? price.replace(",", "") : null;
+                String commaSeperatedString = commaSeperatedParser.matches() ? price.replace(".", "").replace(",", ".") : null;
 
                 String priceString = null;
 
-                if (likelyInternational && euPriceString != null) {
-                    // If the price was guessed to be international and the EU regex passed, its international
-                    if (SignShopConfig.debugging()) SignShop.debugMessage("Actual: INTERNATIONAL");
-                    priceString = euPriceString;
-                } else if (usPriceString != null) {
-                    // If the price was guessed to be not international or the EU regex failed
-                    // AND the US regex passed, its US
-                    if (SignShopConfig.debugging()) SignShop.debugMessage("Actual: NOT INTERNATIONAL");
-                    priceString = usPriceString;
-                }
-
-                if (priceString == null) {
+                if (likelyCommaSeperated && commaSeperatedString != null) {
+                    // If the price was guessed to be comma-seperated and the comma-seperated regex passed, its comma-seperated
+                    if (SignShopConfig.debugging()) SignShop.debugMessage("Actual: COMMA SEPERATED");
+                    priceString = commaSeperatedString;
+                } else if (periodSeperatedString != null) {
+                    // If the price was guessed to be period-seperated or the comma-seperated regex failed
+                    // AND the period-seperated regex passed, its period-seperated
+                    if (SignShopConfig.debugging()) SignShop.debugMessage("Actual: PERIOD SEPERATED");
+                    priceString = periodSeperatedString;
+                } else {
                     // If the price has not yet been parsed, attempt error correction
-                    if (!likelyInternational && euPriceString != null) {
-                        // Detection made a mistake, price was guessed as non-international, but the US regex failed and the EU passed. Its international
-                        if (SignShopConfig.debugging()) SignShop.debugMessage("Actual: INTERNATIONAL");
-                        priceString = euPriceString;
+                    if (!likelyCommaSeperated && commaSeperatedString != null) {
+                        // Detection made a mistake, price was guessed as period-seperated, but the period-seperated regex failed and the comma-seperated passed. Its comma-seperated
+                        if (SignShopConfig.debugging()) SignShop.debugMessage("Actual: COMMA SEPERATED");
+                        priceString = commaSeperatedString;
                     } else {
                         // No valid match could be parsed
                         if (SignShopConfig.debugging()) SignShop.debugMessage("Actual: INVALID");
@@ -175,8 +154,7 @@ public class economyUtil {
         }
 
         // Normalize
-        if(parsedPrice < 0.0d) parsedPrice = 0.0d;
-        if(Double.isNaN(parsedPrice) || Double.isInfinite(parsedPrice)) parsedPrice = 0.0d;
+        if(parsedPrice < 0.0d || Double.isNaN(parsedPrice) || Double.isInfinite(parsedPrice)) parsedPrice = 0.0d;
 
         return parsedPrice;
     }
